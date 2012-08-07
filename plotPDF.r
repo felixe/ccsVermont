a54a4287c9653442f685fad80a19c4481741506d
Felix Erlacher <felix.erlacher@uibk.ac.at>
2012-08-07 13:33:34 +0200

    Added data for Performance Tests, improved test and test description

--- plotPDF.r
+++ plotPDF.r
@@ -2,8 +2,8 @@ csvDir <- "./TestData"
 #
 #get plot label from a given header name
 #
-y_names <- c("cpu0","cpu1","cpu2","cpu3","cpu4","cpu5","cpu6","cpu7","freeMem","totalMem","ObserverProcPack","ObserverProcBytes","ObserverTotPack","pAggExportedEntries","pAggIgnoredPackets","pAggTotalReceivedPackets")
-y_units <- c("User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","Free memory [bytes]","Total memory [bytes]","Processed packets on PCAP [packets/s]","Processed bytes on PCAP [bytes/s]","Total packets on PCAP [packets]","Exported entries in PacketAggregator [entries]","Ignored Packets in PacketAggregator [Packets]","Total received packets in PacketAggregator [packets/s]")
+y_names <- c("cpu0","cpu1","cpu2","cpu3","cpu4","cpu5","cpu6","cpu7","freeMem","totalMem","ObserverProcPack","ObserverProcBytes","ObserverTotPack","pAggExportedEntries","pAggIgnoredPackets","pAggTotalReceivedPackets","Overall CPU Utilization","pcapDropPack","pcapRecPack","pcapTotDropPack","PcapTotRecPack")
+y_units <- c("User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","User CPU usage [%]","Free memory [bytes]","Total memory [bytes]","Processed packets at Observer [packets/s]","Processed bytes at Observer [bytes/s]","Total packets at Observer [packets]","Exported entries in PacketAggregator [entries]","Ignored Packets in PacketAggregator [Packets]","Total received packets in PacketAggregator [packets/s]","CPU usage [%]","Dropped packets at PCAP [packets/s]","Received packets at PCAP [packets/s]","Total dropped packets at PCAP [packets]","Total received packets at PCAP [packets]")
 getylabelfromname <- function(header) {
   index <- 1
   for(i in y_names) {
@@ -50,19 +50,151 @@ myPlot <- function(csvFile,xlabel,title){
 	plot(times,y,col="red",type="l",xlab=xlabel,ylab=getylabelfromname(title))
 }
 
+#
+#do a stack plot for some given files
+#
+dostackplot <- function(fl,title,ylabel) {
+  colors <- c("orange","blue","green","red","yellow","black","brown","pink","blueviolet","burlywood","azure4","coral4");
+#  colors <- c("black","blue3","cornflowerblue","cyan","aquamarine","darkolivegreen1","chartreuse1","chartreuse4");
+  ci <- 1 # current color index  
+  dnames <- c()
+  dcols <- c()
+  
+  #read first data set to get some X/Y ranges
+  fu <- read.delim(fl[1],na.strings="nan",sep=",")
+  fu <- cutdatarows(fu,3,3) # cut of values which are corrupt in some cases
+  
+  u <- fu[,2]
+
+  times <- fu[,1]
+  tmpvals <- u
+  dl <- length(times)
+  
+
+  #iterate over all data sets to determine stack maximum 
+  sum <- seq(0,0,len=dl)
+  for(i in fl) {
+    #get header values
+    u2 <- read.delim(i,na.strings="nan",nrows=2,sep=",")
+    #get name for legend
+    dnames <- c(dnames,sub(".csv","",i))
+
+    #get data values
+    fu <- read.delim(i,na.strings="nan",sep=",")
+    fu <- cutdatarows(fu,3,3)
+
+    u <- fu[,2]
+
+    #sum up data vectors
+    sum <- sum + u
+  }
+  
+  #bring X/Y values to identical sizes
+  pl <- min(length(times),length(tmpvals))
+  times <- times[1:pl]
+  tmpvals <- tmpvals[1:pl]
+
+  timeN <- times-times[1]
+  #set plot ranges
+  minx <- min(timeN,na.rm=TRUE)
+  maxx <- max(timeN,na.rm=TRUE)
+  xlimits <- c(minx,maxx) 
+  miny <- 0
+  maxy <- max(sum,na.rm=TRUE)
+ #maxy <- max(sum,na.rm=TRUE)
+  ylimits <- c(0,maxy)
+
+  plot(timeN,tmpvals,xlim=xlimits,ylim=ylimits,type="n",main=title,xlab="time [s]",ylab=ylabel) #type=n means only axes no x or y drawn
+  
+  #if we plot data from top to bottom we can use the histogram type to create a stack plot...
+  for(i in fl) {
+     dcols <- c(dcols,colors[ci])
+     sum <- sum[1:pl]
+     lines(timeN,sum,col=colors[ci],type="h") #add data set by drawing a line from x to y in type=h ->hystogram style
+     
+     fu <- read.delim(i,na.strings="nan",sep=",") #get next data set
+     fu <- cutdatarows(fu,3,3)
+
+     u <- fu[,2]
+     
+     sum <- sum - u #go down
+     ci <- ci+1 #increment color index
+  } 
+  
+  legend(minx,maxy,dnames, cex=0.8,col=dcols,pch=21:22, lty=1:2,)
+}
+
+#plots a graph with the difference vectors of two given file names
+#plottypes: 0 = both, 1=plot only, 2=boxplot only
+dodiffplot <- function(file1,file2,title,ylabel,plottype) {
+
+  #get data values
+  fu1 <- read.delim(file1,na.strings="nan",sep=",")
+  fu1 <- cutdatarows(fu1,3,3) # cut of values which are corrupt in some cases
+
+  fu2 <- read.delim(file2,na.strings="nan",sep=",")
+  fu2 <- cutdatarows(fu2,3,3) # cut of values which are corrupt in some cases
+
+  u1 <- fu1[,2]
+  u2 <- fu2[,2]
+  times1 <- fu1[,1]
+  times2 <- fu2[,1]
+
+  #check if starting timestamps match
+  #if(times1[1,1] != times2[1,1])
+  #  print("dodiffplot time [s] values are not identical!")
+  
+  #bring X/Y values to identical sizes
+  pl <- min(length(fu1[,1]),length(fu2[,1]),length(u1),length(u2))
+  times <- fu1[,1]
+  times <- times[1:pl]  
+  vals1 <- u1
+  vals1 <- vals1[1:pl]
+  vals2 <- u2
+  vals2 <- vals2[1:pl]
+
+  vals3 <- vals1-vals2
+
+  normalizedTimes <- times-times[1]
+
+  if(plottype == 0 || plottype == 1) {
+    plot(normalizedTimes,vals3,col="red",type="l",main=title,xlab="time [s]",ylab=ylabel)
+  }
+  if(plottype == 0 || plottype == 2) {
+    boxplot(vals3,main="tscheckTscheck",xlab="time [s]",ylab=ylabel);
+  }
+}
+
 #------------
 #MAIN
 #-----------
-fl <- list.files(path=csvDir,recursive = FALSE, pattern=".*csv$")
-setwd(csvDir)
-for(i in fl){
-	#filePath <-paste(csvDir,i,sep="/")
-	if(file.info(i)$size!=0){
-		flname<-i
-		#substitute .csv with nothing	
-		flname<-sub(".csv","",flname)
-#		write(flname,stdout())
-		myPlot(i,"Time[s]",flname)
+	fl <- list.files(path=csvDir,recursive = FALSE, pattern=".*csv$")
+	setwd(csvDir)
+	
+        cat(names(fl),sep="\n")
+	#do simple plot for all files
+	for(i in fl){
+		#filePath <-paste(csvDir,i,sep="/")
+		if(file.info(i)$size!=0){
+			flname<-i
+			#substitute .csv with nothing	
+			flname<-sub(".csv","",flname)
+	#		write(flname,stdout())
+			myPlot(i,"Time[s]",flname)
+		}
 	}
-}
-dev.off()
+
+	#do stackplot for all cpu files
+	#all cpu files from working dir, changed already above
+	cpufiles <- list.files(getwd(),recursive = FALSE, pattern="cpu.*csv$")
+	pdf("OverallCPU.pdf",paper="a4r",width=50,height=20)
+	par(mfcol=c(1,1))
+	dostackplot(cpufiles,"Overall CPU Utilization","CPU Usage [%]")
+	dev.off()
+	
+	totDropFile <- "pcapDropPack.csv"
+	totRecFile <- "pcapRecPack.csv"
+	pdf("pcapProcPack.pdf",paper="a4r", width=50, height=20)
+	par(mfcol=c(1,1))
+	dodiffplot(totRecFile,totDropFile,"Processed Packets on PCAP","Processed packets [packets/s]",1)
+	dev.off()
--- readVals.sh
+++ readVals.sh
@@ -6,16 +6,24 @@ $xml = new XML::Simple;
 
 
 
-
+#>> heist zum anhängen öffnen
 while (1) {
-	open(cpu0, ">>./TestData/cpu0.csv");
-	open(cpu1, ">>./TestData/cpu1.csv");
-	open(cpu2, ">>./TestData/cpu2.csv");
-	open(cpu3, ">>./TestData/cpu3.csv");
-	open(cpu4, ">>./TestData/cpu4.csv");
-	open(cpu5, ">>./TestData/cpu5.csv");
-	open(cpu6, ">>./TestData/cpu6.csv");
-	open(cpu7, ">>./TestData/cpu7.csv");
+	open(cpu0, ">>./TestData/cpu00.csv");
+	open(cpu1, ">>./TestData/cpu01.csv");
+	open(cpu2, ">>./TestData/cpu02.csv");
+	open(cpu3, ">>./TestData/cpu03.csv");
+	open(cpu4, ">>./TestData/cpu04.csv");
+	open(cpu5, ">>./TestData/cpu05.csv");
+	open(cpu6, ">>./TestData/cpu06.csv");
+	open(cpu7, ">>./TestData/cpu07.csv");
+	open(cpu8, ">>./TestData/cpu08.csv");
+	open(cpu9, ">>./TestData/cpu09.csv");
+	open(cpu10, ">>./TestData/cpu10.csv");
+	open(cpu11, ">>./TestData/cpu11.csv");
+	open(pcapDrop, ">>./TestData/pcapDropPack.csv");
+	open(pcapTotDrop, ">>./TestData/pcapTotDropPack.csv");
+	open(pcapRec, ">>./TestData/pcapRecPack.csv");
+	open(pcapTotRec, ">>./TestData/pcapTotRecPack.csv");
 	open(freeMem, ">>./TestData/freeMem.csv");
 	open(totalMem, ">>./TestData/totalMem.csv");
 	open(procPack, ">>./TestData/ObserverProcPack.csv");
@@ -38,6 +46,20 @@ while (1) {
 
 	print cpu0 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{0}->{util}->[1]->{content}\n";
 	print cpu1 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{1}->{util}->[1]->{content}\n";
+	print cpu2 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{2}->{util}->[1]->{content}\n";
+	print cpu3 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{3}->{util}->[1]->{content}\n";
+	print cpu4 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{4}->{util}->[1]->{content}\n";
+	print cpu5 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{5}->{util}->[1]->{content}\n";
+	print cpu6 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{6}->{util}->[1]->{content}\n";
+	print cpu7 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{7}->{util}->[1]->{content}\n";
+	print cpu8 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{8}->{util}->[1]->{content}\n";
+	print cpu9 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{9}->{util}->[1]->{content}\n";
+	print cpu10 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{10}->{util}->[1]->{content}\n";
+	print cpu11 "$data->{sensorData}->{epochtime},$data->{sensorData}->{processor}->{11}->{util}->[1]->{content}\n";
+	print pcapDrop "$data->{sensorData}->{epochtime},$data->{sensorData}->{sensor}->{observer}->{addInfo}->{pcap}->{dropped}->{content}\n";
+	print pcapTotDrop "$data->{sensorData}->{epochtime},$data->{sensorData}->{sensor}->{observer}->{addInfo}->{pcap}->{totalDropped}->{content}\n";
+	print pcapRec "$data->{sensorData}->{epochtime},$data->{sensorData}->{sensor}->{observer}->{addInfo}->{pcap}->{received}->{content}\n";
+	print pcapTotRec "$data->{sensorData}->{epochtime},$data->{sensorData}->{sensor}->{observer}->{addInfo}->{pcap}->{totalReceived}->{content}\n";
 	print freeMem "$data->{sensorData}->{epochtime},$data->{sensorData}->{memory}->{free}->{content}\n";
 	print totalMem "$data->{sensorData}->{epochtime},$data->{sensorData}->{memory}->{total}->{content}\n";
 	print procPack "$data->{sensorData}->{epochtime},$data->{sensorData}->{sensor}->{observer}->{addInfo}->{observer}->{processed}->[1]->{content}\n";
