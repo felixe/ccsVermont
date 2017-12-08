7ac606d93045c13df9d992f5f27d7dadbee626c5
Felix Erlacher <felix.erlacher@ccs-labs.org>
2017-12-08 10:10:56 +0100

    rm useless file

--- relink.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# Link vermont so it can use libzero. (hacky; should be done by cmake)
-# usage: 1) run make 2) run this to link again
-
-/usr/bin/c++ CMakeFiles/vermont.dir/src/vermont.o -o vermont -rdynamic -L/home/martin/vermont/src/modules -L/home/martin/vermont/src/core -L/home/martin/vermont/src/common/anon -L /home/martin/vermont/src/common/ipfixlolib -L/home/martin/vermont/src/common -L/home/martin/vermont/src/osdep -L/usr/local/lib src/modules/libmodules.a src/core/libcore.a src/common/anon/libanon.a src/common/ipfixlolib/libipfixlolib.a src/common/libcommon.a src/osdep/libosdep.a /home/martin/ntop/userland/lib/libpfring.a -lpthread -lboost_regex-mt -lboost_filesystem-mt -lboost_system-mt -lxml2 -lpcap /usr/local/lib/libpcap.so /usr/local/lib/libpcap.so -Wl,-rpath,/usr/local/lib:
